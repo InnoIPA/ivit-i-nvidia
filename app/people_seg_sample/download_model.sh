59ce6db6e30cad904b0bf850abe99c603d67557a
MaxChangInnodisk <max_chang@innodisk.com>
2022-05-24 17:44:51 +0800

    feat: auto move to the correct directory

--- app/people_seg_sample/download_model.sh
+++ app/people_seg_sample/download_model.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+echo "$(date +"%F %T") Download model from google drive ..."
+ROOT=$(dirname `realpath ${0}`)
+echo $ROOT
+cd $ROOT
 
 # model
 wget https://api.ngc.nvidia.com/v2/models/nvidia/tao/peoplesegnet/versions/deployable_v2.0.1/files/peoplesegnet_resnet50.etlt
