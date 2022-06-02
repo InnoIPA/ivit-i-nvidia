f48791226548d954f219a3a7fcc3d09296c3950d
MaxChangInnodisk <max_chang@innodisk.com>
2022-06-02 09:17:17 +0800

    feat: merge several utils into common

--- init_i/web/utils/uuid.py
+++
@@ -1,7 +0,0 @@
-import uuid
-
-def gen_uuid(name:str, len:int=8) -> str:
-    """
-    Generate uuid for each application
-    """
-    return str(uuid.uuid4())[:len]
--- init_i/web/utils/v4l2.py
+++
@@ -1,6 +0,0 @@
-import subprocess
-
-def get_v4l2() -> list:
-    ret = subprocess.run("ls /dev/video*",  text=True, shell=True, stdout=subprocess.PIPE).stdout
-    ret_list = ret.strip().split('\n')
-    return ret_list
