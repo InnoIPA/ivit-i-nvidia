5f47084e0a489039c6bded11cdb7c1bba2d440b2
MaxChangInnodisk <max_chang@innodisk.com>
2022-06-02 09:21:15 +0800

    feat: change app to task and provide a easier key for framework

--- init_i/web/api/basic_setting.py
+++ init_i/web/api/basic_setting.py
@@ -2,11 +2,12 @@ class basic_setting(object):
     JSONIFY_PRETTYPRINT_REGULAR=True
     JSON_SORT_KEYS=False
     TASK_ROOT="./app"
-    TASK_CFG_NAME="app.json"
+    TASK_CFG_NAME="task.json"
     TASK=dict()
     TASK_LIST=dict()
     UUID=dict()
     RE_UUID=dict()
     SRC=dict()
     ALLOWED_HOSTS = ['*']
-    RE_SRC='0'
+    RE_SRC='0',
+    AF='tensorrt'
