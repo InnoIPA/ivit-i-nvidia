4287c2853e37e5b366568109cffddc4340c2a2a8
MaxChangInnodisk <max_chang@innodisk.com>
2022-06-20 16:05:54 +0800

    style: remove unused script

--- init_i/app/counting.py
+++ init_i/app/counting.py
@@ -20,12 +20,6 @@ class Counting(App):
 
     def __call__(self, frame, info):
         """
-        1. Get all the bounding box and calculate the center point.
-        2. Saving the center point and copy a preview one in the last. ("self.cnt_pts_cur_frame", "self.cnt_pts_prev_frame").
-        3. Calculate the distance between current and preview center point. ( via math.hypot ).
-        4. If the distance smaller than the limit_distance than we updating it in "track_obj" and remove from the "self.cnt_pts_cur_frame".
-        5. The remaining items in "self.cnt_pts_cur_frame" is the new one, add to "track_obj".
-        6. Draw the information in it.
         """
         # clear data
         for key in self.cnt_pts_cur_frame:
@@ -72,67 +66,4 @@ class Counting(App):
             wid, hei = get_text_size(cnt)
             cv2.putText(frame, cnt, (10, 10+hei+(hei*(label_num+1))+(hei*label_num) ), FONT, FONT_SCALE, COLOR, FONT_THICKNESS)
 
-        # # if the first frame web have to saving all object here
-        # if self.frame_idx <= 1:  
-        #     for label in self.detected_labels:
-        #         for pt in self.cnt_pts_cur_frame[label]:
-        #             for pt2 in self.cnt_pts_prev_frame[label]:
-                        
-        #                 # calculate the distance, if smaller then limit_distance, then it might the same one
-        #                 if get_distance(pt, pt2) < self.limit_distance:
-        #                     self.track_obj[label][ self.track_idx[label] ]=pt
-        #                     self.track_idx[label] +=1
-
-        # # if not the firt frame, we update the center point and separate the new one
-        # else:
-        #     for label in self.detected_labels:
-
-        #         track_obj_copy = self.track_obj[label].copy()
-        #         self.cnt_pts_cur_frame_copy = self.cnt_pts_cur_frame[label].copy()
-                
-        #         for idx, pt2 in track_obj_copy.items():
-                    
-        #             # if object not exist we have to remove the the disappear one
-        #             obj_exist = False
-
-        #             for pt in self.cnt_pts_cur_frame_copy:                
-                        
-        #                 # calculate the distance, if the some one we have to update the center point
-        #                 if get_distance(pt, pt2) < self.limit_distance:
-        #                     self.track_obj[label][idx]=pt
-                            
-        #                     if pt in self.cnt_pts_cur_frame[label]:
-        #                         self.cnt_pts_cur_frame[label].remove(pt)
-        #                     obj_exist = True
-
-        #             if not obj_exist:
-        #                 self.track_obj[label].pop(idx)
-
-        # # adding the remaining point to track_obj
-        # for label_num, label in enumerate(self.detected_labels):
-
-        #     for pt in self.cnt_pts_cur_frame[label]:
-        #         self.track_obj[label][ self.track_idx[label] ]=pt
-        #         self.track_idx[label] +=1
-            
-        #     # draw the number text on frame
-        #     for idx, pt in self.track_obj[label].items():
-        #         wid, hei = get_text_size(str(idx))
-        #         cv2.putText(frame, str(idx), (pt[0]-(wid//2), pt[1]+(hei//2)), FONT, FONT_SCALE, COLOR, FONT_THICKNESS)
-
-        #     cur_total_num = list(self.track_obj[label].keys())
-        #     # update key
-        #     if not label in self.total_num: 
-        #         self.total_num.update( { label: 0 } )
-        #     # update total number
-        #     self.total_num[label] = cur_total_num[-1] if cur_total_num != [] else self.total_num[label]
-                
-        #     content = f"Detected {self.total_num[label]} {label}"
-        #     wid, hei = get_text_size(content)
-        #     cv2.putText(frame, content, (10, 10+(hei*(label_num+1))+(10*label_num) ), FONT, FONT_SCALE, COLOR, FONT_THICKNESS)
-
-        #     # update the preview information
-        #     self.cnt_pts_prev_frame[label] = self.cnt_pts_cur_frame[label].copy()
-
-        # return frame
         return frame
