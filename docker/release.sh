17fff3eaeb23d5ab51257b18693f379b04cc5217
MaxChangInnodisk <max_chang@innodisk.com>
2022-06-27 14:45:29 +0800

    feat: add release script and dockerfile

---
+++ docker/release.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+ROOT=$(dirname `realpath $0`)
+source "${ROOT}/utils.sh"
+
+# Install pre-requirement
+if [[ -z $(which jq) ]];then
+    printd "Installing requirements .... " Cy
+    sudo apt-get install jq -yqq
+fi
+
+# Variable
+CONF="init-i.json"
+FLAG=$(ls ${CONF} 2>/dev/null)
+if [[ -z $FLAG ]];then
+    CONF="${RUN_PWD}/${CONF}"
+    FLAG=$(ls ${CONF} 2>/dev/null)
+    if [[ -z $FLAG ]];then
+        printd "Couldn't find configuration (${CONF})" Cy
+        exit
+    fi
+else
+    printd "Detected configuration (${CONF})" Cy
+fi
+
+# Parse information from configuration
+BASE_NAME=$(cat ${CONF} | jq -r '.PROJECT')
+TAG_VER=$(cat ${CONF} | jq -r '.VERSION')
+TAG_PLATFORM=$(cat ${CONF} | jq -r '.PLATFORM')
+
+# Concate name
+IMAGE_NAME="${BASE_NAME}/${TAG_PLATFORM}:${TAG_VER}"
+printd "Concatenate docker image name: ${IMAGE_NAME}" Cy
+
+# Change the default runtime
+printd "Change the docker runtime to NVIDIA" Cy
+# cd $ROOT
+sudo python3 "${ROOT}/docker_daemon.py"
+sudo systemctl restart docker
+
+# Build the docker image
+printd "Build the docker image. (${IMAGE_NAME})" Cy
+docker build -t ${IMAGE_NAME} -f "${ROOT}/DockerfileRelease" .
