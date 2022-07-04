5fe77aeead9e63ed0327d4857702e02882591d48
MaxChangInnodisk <max_chang@innodisk.com>
2022-07-04 18:18:44 +0800

    feat: add try exception

--- demo.py
+++ demo.py
@@ -1,4 +1,5 @@
 import os, sys, cv2, logging, argparse
+from pyexpat import model
 from shutil import ExecError
 import numpy as np
 sys.path.append(os.getcwd())
@@ -9,114 +10,102 @@ from init_i.utils.drawing_tools import Draw, get_palette, draw_fps
 from init_i.common import api
 from init_i.web.ai.pipeline import Source
 from init_i.app.handler import get_application
+from init_i.web.tools.common import handle_exception
 
 CV_WIN='Detection Results'
 
 def main(args):
 
-    # ----------------------------------------------------------------------------------------------------------------
     # 1. Initialize logger
     config_logger(log_name='init-i-nvidia.log', write_mode='w', level='debug')
 
-    # ----------------------------------------------------------------------------------------------------------------
-    # 2. Load and parse configuration
+    # 2. Load and combine configuration
     app_conf = load_json(args.config)                       # load the configuration of the application
     model_conf = load_json(app_conf['prim']['model_json'])             # load the configuration of the AI model
-    model_conf.update(app_conf)
+    total_conf = model_conf.copy()
+    total_conf.update(app_conf)
 
-    # ----------------------------------------------------------------------------------------------------------------
-    # 3. Check the framework is tensorrt
-    if app_conf['framework'].lower() != 'tensorrt':
-        msg = 'Excepted a tensorrt platform, but got {}.'.format('tensorrt')
-        logging.error(msg)
-        raise Exception(msg)
-
-    # ----------------------------------------------------------------------------------------------------------------
-    # 4. Get the target API
+    # 3. Get the target API and Draw tool
     try:
-        trg = api.get(model_conf)
+        trg = api.get(total_conf)
+        draw = Draw()
     except Exception as e:
-        raise Exception('LOAD API FAILED: {}'.format(e))
-        
-    draw = Draw()
-    # ----------------------------------------------------------------------------------------------------------------
-    # 5. Load and initialize model which return a list of objects for inference and the palette
-    trt_objects, palette = trg.load_model(model_conf)
-    
-    # ----------------------------------------------------------------------------------------------------------------
-    # 6. Start inference base on three mode
-    source_type = parse_input_data(model_conf['source'])
+        handle_exception(error=e, title="Could not get init-i API", exit=True)
 
-    src = Source(model_conf['source'], model_conf['source_type'])
-
-    has_application=False
-    
+    # 4. Load and initialize model which return a list of objects for inference and the palette
     try:
-        application = get_application(model_conf)
-        has_application = False if application == None else True
+        trt_objects, palette = trg.load_model(total_conf)
     except Exception as e:
-        logging.error(e)
-        has_application=False
-    
+        handle_exception(error=e, title="Could not load AI model", exit=True)
+
+    # 5. Start inference base on three mode
+    src = Source(total_conf['source'], total_conf['source_type'])
+
+    # 6. Setting Application
+    has_app=False
     try:
-        app_info = model_conf["application"]
-        
+        application = get_application(total_conf)
+        has_app = False if application == None else True
+
+        # Setup parameter if needed
+        app_info = total_conf["application"]
+
+        # Area detection: point_points
         if "area" in app_info["name"]:
             key = "area_points"
-            if not key in app_info:
-                ret_frame, frame = src.get_frame()
-                if ret_frame:
-                    application.set_area(pnts=None, frame=frame)
-            else:
-                application.set_area(pnts=app_info[key])
-            
-    except Exception as e:
-        exc_type, exc_obj, exc_tb = sys.exc_info()
-        fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
-        msg = 'Stream Error: \n{}\n{} ({}:{})'.format(exc_type, exc_obj, fname, exc_tb.tb_lineno)
-        logging.error(msg)
+            if not key in app_info: application.set_area(pnts=None, frame=src.get_first_frame())
+            else: application.set_area(pnts=app_info[key])   
 
+    except Exception as e:
+        handle_exception(error=e, title="Could not load application ... set app to None", exit=False)
+        has_app=False
+    
+    # 7. Start inference
+    
+    # server mode: won't display cv window
     if not args.server:
         cv2.namedWindow(CV_WIN, cv2.WND_PROP_FULLSCREEN)
         cv2.setWindowProperty(CV_WIN, cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
+
     try:
         while True:
+
+            # 7.1. capture frame from source object
             ret_frame, frame = src.get_frame()
-            if not ret_frame:
-                if model_conf["source_type"] in ['Image', 'V4L2']:
-                    break
-                elif model_conf["source_type"] in ['Video', 'RTSP']:
-                    src = Source(model_conf['source'], model_conf['source_type'])
-                    continue
-
-            org_frame = frame.copy()
-            info = trg.inference(trt_objects, org_frame, model_conf)   
-
-            if args.server:
-                logging.info( info["detections"])
-            else:
-                if info is not None:
-                    if not has_application:
-                        frame = draw.draw_detections(info, palette, model_conf)
-                    else:
-                        frame = application(org_frame, info)
-                else:
-                    continue
-                
-                cv2.imshow(CV_WIN, frame)
             
+            # 7.2. if not frame break
+            if not ret_frame: break
+
+            # 7.3. do inference
+            org_frame = frame.copy()
+            info = trg.inference(trt_objects, org_frame, total_conf)   
+
+            # 7.4. draw results or using application
+            if not args.server:
+                
+                if info is None: continue
+
+                # using default draw tool if no application available
+                if not has_app:
+                    frame = draw.draw_detections(info, palette, total_conf)
+                else:
+                    frame = application(org_frame, info)
+
+                # show the results            
+                cv2.imshow(CV_WIN, frame)
                 key = cv2.waitKey(1 if not args.debug else 0)
                 if key in {ord('q'), ord('Q'), '27'}:
-                    break
-                else:
-                    pass
+                    break 
+
+            else:
+                logging.info( info["detections"])
 
     except KeyboardInterrupt:
         logging.warning("Quit")
     finally:        
+        # release if needed
         trg.release()
-        if not args.server:
-            src.release()
+        if not args.server: src.release()
 
 
 if __name__ == "__main__":
